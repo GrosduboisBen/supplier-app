c1aed8e4cc78f9b3ff919a8fb050bb941e0c5a62
benoit.gros-dubois <benoit.gros-dubois@polynom.io>
2025-08-23 16:03:28 +0200

    update angular cli  to 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,32 +6,25 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CompanyListComponent } from './components/companies/company-list/company-list.component';
 import { MatCardModule } from '@angular/material/card';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { MatTableModule } from '@angular/material/table';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { ProviderComponent } from './components/provider/provider.component';
 import { MissionsComponent } from './components/missions/missions.component';
 import { OrdersComponent } from './components/orders/orders.component';
 import { EvaluationsComponent } from './components/evaluations/evaluations.component';
-@NgModule({
-  declarations: [
-    AppComponent,
-    CompanyListComponent,
-    ProjectsComponent,
-    ProviderComponent,
-    MissionsComponent,
-    OrdersComponent,
-    EvaluationsComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatCardModule,
-    HttpClientModule,
-    MatTableModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        CompanyListComponent,
+        ProjectsComponent,
+        ProviderComponent,
+        MissionsComponent,
+        OrdersComponent,
+        EvaluationsComponent,
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatCardModule,
+        MatTableModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
--- src/app/components/companies/company-list/company-list.component.spec.ts
+++ src/app/components/companies/company-list/company-list.component.spec.ts
@@ -1,7 +1,8 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
 import { CompanyService } from 'src/app/services/company.service';
 import { Company } from 'src/app/models/company';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 describe('CompanyService', () => {
   let service: CompanyService;
@@ -10,9 +11,9 @@ describe('CompanyService', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
-      providers: [CompanyService],
-    });
+    imports: [],
+    providers: [CompanyService, provideHttpClient(withInterceptorsFromDi()), provideHttpClientTesting()]
+});
     service = TestBed.inject(CompanyService);
     httpMock = TestBed.inject(HttpTestingController);
   });
