0e8bb73b04cc678c4b0a3282c5b62511c8f2893a
benoit.gros-dubois <benoit.gros-dubois@polynom.io>
2025-08-23 16:30:22 +0200

    chores: clean comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,6 @@ import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
 import { RouterModule } from '@angular/router';
-// import { CompanyListComponent } from './components/companies/company-list/company-list.component';
-// import { ProjectsComponent } from './components/projects/projects.component';
-// import { EvaluationsComponent } from './components/evaluations/evaluations.component';
-// import { OrdersComponent } from './components/orders/orders.component';
-// import { MissionsComponent } from './components/missions/missions.component';
-// import { ProviderComponent } from './components/provider/provider.component';
 
 @Component({
   selector: 'app-root',
@@ -16,13 +10,6 @@ import { RouterModule } from '@angular/router';
     MatCardModule,
     MatTableModule,
     RouterModule,
-    // AppComponent,
-    // CompanyListComponent,
-    // ProjectsComponent,
-    // ProviderComponent,
-    // MissionsComponent,
-    // OrdersComponent,
-    // EvaluationsComponent,
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
