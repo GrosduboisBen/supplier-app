2267c22c7dd7d293e94acfdd886c32b0523a9f29
benoit.gros-dubois <benoit.gros-dubois@polynom.io>
2025-08-24 14:53:23 +0200

    feat: evaluation style

--- src/app/components/evaluations/evaluations.component.ts
+++ src/app/components/evaluations/evaluations.component.ts
@@ -1,35 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { MatTableDataSource } from '@angular/material/table';
-import { EvaluationService } from 'src/app/services/evaluation.service';
-import { Evaluation } from 'src/app/models/evaluation';
 import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
 import { MATERIAL_IMPORTS } from 'material.import';
+import { Observable } from 'rxjs';
+import { Evaluation } from 'src/app/models/evaluation';
+import { EvaluationService } from 'src/app/services/evaluation.service';
 
 @Component({
-    selector: 'app-evaluations',
-    templateUrl: './evaluations.component.html',
-    styleUrls: ['./evaluations.component.scss'],
-    standalone: true,
-    imports: [
-      CommonModule,
-      ...MATERIAL_IMPORTS
-    ],
+  selector: 'app-evaluation',
+  templateUrl: './evaluations.component.html',
+  styleUrls: ['./evaluations.component.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    ...MATERIAL_IMPORTS
+  ],
 })
 export class EvaluationsComponent implements OnInit {
-  displayedColumns: string[] = ['id', 'rating', 'comment', 'missionId'];
-  dataSource = new MatTableDataSource<Evaluation>();
-
-  constructor(private evaluationService: EvaluationService) {}
+  evaluations$!: Observable<Evaluation[]>;
+  evaluationService: EvaluationService = inject(EvaluationService);
 
   ngOnInit(): void {
-    this.evaluationService.getEvaluations().subscribe((evaluations) => {
-      this.dataSource.data = evaluations;
-    });
-  }
-
-  deleteEvaluation(id: number): void {
-    this.evaluationService.deleteEvaluation(id).subscribe(() => {
-      this.dataSource.data = this.dataSource.data.filter(e => e.id !== id);
-    });
+    this.evaluations$ = this.evaluationService.getEvaluations();
   }
 }
