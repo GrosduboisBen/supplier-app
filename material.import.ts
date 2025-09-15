9cbcba3b7fd395745541c2430d69c8ca6e58b5a7
benoit.gros-dubois <benoit.gros-dubois@polynom.io>
2025-09-15 13:35:35 +0200

    update: select material imports

--- material.import.ts
+++ material.import.ts
@@ -4,6 +4,8 @@ import { MatButtonModule } from '@angular/material/button';
 import {MatGridListModule} from '@angular/material/grid-list';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
 
 export const MATERIAL_IMPORTS = [
   MatCardModule,
@@ -14,5 +16,7 @@ export const MATERIAL_IMPORTS = [
 
 export const MATERIAL_DIALOGS_IMPORTS = [
   MatFormFieldModule,
-  MatInputModule
+  MatInputModule,
+  MatSelectModule,
+  MatOptionModule
 ]
