b1f06c214da1b25b08b79916670d095a6942e903
benoit.gros-dubois <benoit.gros-dubois@polynom.io>
2025-09-15 13:57:03 +0200

    update: complete project service

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -8,11 +8,27 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root'
 })
 export class ProjectService {
-  private baseUrl = `${environment.apiURL}/projects`;
+  private readonly apiUrl = `${environment.apiURL}/projects`;
 
   constructor(private http: HttpClient) {}
 
-  getProjects(): Observable<Project[]> {
-    return this.http.get<Project[]>(this.baseUrl);
+  getAll(): Observable<Project[]> {
+    return this.http.get<Project[]>(this.apiUrl);
+  }
+
+  getById(id: number): Observable<Project> {
+    return this.http.get<Project>(`${this.apiUrl}/${id}`);
+  }
+
+  create(payload: Omit<Project, 'id'>): Observable<Project> {
+    return this.http.post<Project>(this.apiUrl, payload);
+  }
+
+  update(id: number, payload: Partial<Project>): Observable<Project> {
+    return this.http.put<Project>(`${this.apiUrl}/${id}`, payload);
+  }
+
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
   }
 }
