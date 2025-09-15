7c11998c63a0b3500dab00e4275377ead67263fc
benoit.gros-dubois <benoit.gros-dubois@polynom.io>
2025-09-15 14:00:05 +0200

    feat: base create provider form component

---
+++ src/app/dialogs/provider-create/provider-create.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-provider-create',
+  imports: [],
+  templateUrl: './provider-create.component.html',
+  styleUrl: './provider-create.component.scss'
+})
+export class ProviderCreateComponent {
+
+}
