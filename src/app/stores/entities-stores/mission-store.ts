3168cbfa41abf47cc2ebdb9b803e934612df4e1c
benoit.gros-dubois <benoit.gros-dubois@polynom.io>
2025-09-15 14:55:29 +0200

    feat: mission entity store

---
+++ src/app/stores/entities-stores/mission-store.ts
@@ -0,0 +1,14 @@
+// company.store.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { environment } from 'src/environments/environment';
+import { EntityStore } from './global';
+import { Mission } from 'src/app/models/mission';
+
+@Injectable({ providedIn: 'root' })
+export class CompanyStore extends EntityStore<Mission> {
+  constructor(http: HttpClient) {
+    super(http, `${environment.apiURL}/missions`);
+  }
+}
