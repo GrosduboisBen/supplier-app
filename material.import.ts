c340cbf230429fd99c0af802e0a5c4a892c29e68
benoit.gros-dubois <benoit.gros-dubois@polynom.io>
2025-09-15 15:22:53 +0200

    update: material import dates modules

--- material.import.ts
+++ material.import.ts
@@ -6,17 +6,21 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatOptionModule } from '@angular/material/core';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 export const MATERIAL_IMPORTS = [
   MatCardModule,
   MatTableModule,
   MatButtonModule,
-  MatGridListModule
+  MatGridListModule,
 ];
 
 export const MATERIAL_DIALOGS_IMPORTS = [
   MatFormFieldModule,
   MatInputModule,
   MatSelectModule,
-  MatOptionModule
+  MatOptionModule,
+  MatDatepickerModule,
+  MatNativeDateModule
 ]
