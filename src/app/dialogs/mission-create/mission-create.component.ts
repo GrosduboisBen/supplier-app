ebda543a1c2b9ced29b5e79b7db74d5d4df83b1d
benoit.gros-dubois <benoit.gros-dubois@polynom.io>
2025-09-15 14:55:45 +0200

    feat: base create mission form component

---
+++ src/app/dialogs/mission-create/mission-create.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mission-create',
+  imports: [],
+  templateUrl: './mission-create.component.html',
+  styleUrl: './mission-create.component.scss'
+})
+export class MissionCreateComponent {
+
+}
